986f3b875c7b890a4495e1ae6471bda782cc5b58
meanzza123 <33210135+meanzza123@users.noreply.github.com>
2020-05-11 20:59:54 +0700

    commit/redux/axios

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
 import App from './App';
+import rootReducer from './reducers';
+import { fetchAllPosts } from './actions/index';
 import * as serviceWorker from './serviceWorker';
+import logger from 'redux-logger'
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+store.dispatch(fetchAllPosts());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
